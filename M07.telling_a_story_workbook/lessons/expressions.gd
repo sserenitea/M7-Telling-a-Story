14ccfe5a02a44e078e53ca3875c0840ff2e84fb4
sserenitea <jeyang1@s.sfusd.edu>
2025-11-07 14:52:22 -0800

    L7 done

---
+++ M07.telling_a_story_workbook/lessons/expressions.gd
@@ -0,0 +1,9 @@
+extends Control
+
+@onready var body: TextureRect = %Body
+@onready var expressions: TextureRect = %Expressions
+@onready var button_sophia: Button = %ButtonSophia
+@onready var button_pink: Button = %ButtonPink
+@onready var button_regular: Button = %ButtonRegular
+@onready var button_sad: Button = %ButtonSad
+@onready var button_happy: Button = %ButtonHappy
